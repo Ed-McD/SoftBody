713cd6cdd83db5f8245fd033432ee5c8fda22672
Ed-McD <edard_666@hotmail.co.uk>
2015-02-09 13:21:55 +0000

    Project with Dynamic VB

---
+++ SimWorlds DXBase/Game/VBPLane.h
@@ -0,0 +1,30 @@
+#ifndef _VB_PLANE_H_
+#define _VB_PLANE_H_
+#include "VBGO.h"
+#include "vertex.h"
+//procedurally generate a VBGO Cube
+//each side be divided in to _size * _size squares (2 triangles per square)
+
+class VBPlane : public VBGO
+{
+public:
+	
+	VBPlane(){};
+	virtual ~VBPlane(){};
+
+	//initialise the Veretx and Index buffers for the cube
+	void init(int _size, ID3D11Device* _GD);
+	void Tick(GameData* GD);
+
+	virtual void Draw(DrawData* _DD);
+
+protected:
+	//this is to allow custom versions of this which create the basic cube and then distort it
+	//see Spirla, SpikedVB and Pillow
+	virtual void Transform(){};
+
+	int m_size;
+	myVertex* m_vertices;
+};
+
+#endif
